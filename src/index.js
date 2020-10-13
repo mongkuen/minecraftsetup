b27818005e60d3eb46c64a259df4e412c0e50693
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2020-10-12 22:47:36 -0500

    Add web page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { getDnsmasqIp } = require('./utils/getDnsmasqIp')
 const { getHomeRecord } = require('./utils/getHomeRecord')
 const { recreateHomeRecord } = require('./utils/recreateHomeRecord')
 const { restartDockerDns } = require('./utils/restartDockerDns')
+const http = require('http')
 
 const updateNetlifyDns = async () => {
   logDnsCheck(null, '***** STARTING... *****')
@@ -34,8 +35,24 @@ const updateNetlifyDns = async () => {
 }
 
 const init = async () => {
-  const twentyMinutes = 1000 * 60 * 20
+  const port = 80
+  const app = http.createServer(async (_, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    const ip = await getIp()
+    res.end(`Setting up Switch Minecraft
+1. System Settings
+2. Internet
+3. Internet Settings > Access your connected network
+4. Change Settings
+5. DNS Settings > Choose Manual
+6. Primary DNS > ${ip}
+7. Secondary DNS > 8.8.8.8
+    `)
+  })
+  app.listen(port)
+  console.log(`Diagnostic page running on port ${port}`)
 
+  const twentyMinutes = 1000 * 60 * 20
   await updateNetlifyDns()
   setInterval(async () => {
     await updateNetlifyDns()
